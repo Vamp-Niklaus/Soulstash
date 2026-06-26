62eb589527c2592b511e7f54d59ee057fe880d25
Vamp-Niklaus <rakeshkumarjnv7364@gmail.com>
2026-06-26 12:56:21 +0530

    Deployment: Add GitHub Actions workflow for automated GitHub Pages hosting

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: 'spa',
+  base: process.env.GITHUB_ACTIONS ? '/Soulstash/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
